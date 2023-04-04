1f0b41a6b42d61e74ad6d4666f935172647f19a7
Bizarre <la_insolence@mail.ru>
2023-04-04 22:19:15 +0300

    plus timer

--- browser/index.ts
+++ browser/index.ts
@@ -9,19 +9,43 @@ const ctx = canvas.getContext("2d")!;
 let initialState = new GameState({height: 20, width: 20});
 Game.addFood(initialState);
 const game = new Game(initialState);
+let gameStarted = false;
+let startTime = 0;
+let lastStepTime = 0;
+const gameStartButton = document.getElementById("start");
+
+function onGameStart() {
+    gameStarted = true;
+    window.requestAnimationFrame(renderFrame);
+}
+
+function renderFrame(timeStamp: DOMHighResTimeStamp) {
+    if (game.over()) {
+        alert('GAME OVER!!!!!');
+        game.reset();
+        randy.draw(game.getCurrentState());
+        return;
+    }
+    if (gameStarted) {
+        gameStarted = false;
+        lastStepTime = timeStamp;
+        randy.draw(game.getCurrentState());
+    }
+    if (timeStamp - lastStepTime > 500) {
+        game.advance();
+        randy.draw(game.getCurrentState());
+        lastStepTime = timeStamp;
+    }
+    window.requestAnimationFrame(renderFrame);
+}
+
+gameStartButton?.addEventListener('click', onGameStart);
 
 const randy = new Render(ctx, 20);
 randy.draw(game.getCurrentState());
 
 function step(vector: Point): void {
     game.setCurrentDirection(vector);
-    game.advance();
-    randy.draw(game.getCurrentState());
-    if (game.over()) {
-        alert('GAME OVER!!!!!');
-        game.reset();
-        randy.draw(game.getCurrentState());
-    }
 }
 
 function addOnClick(id: string, vector: Point): void {
