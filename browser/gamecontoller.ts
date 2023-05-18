2586d6a5bdd031e188f215441ed92d1dfde8d79b
Bizarre <la_insolence@mail.ru>
2023-05-18 22:02:18 +0300

    changed game controller

--- browser/gamecontoller.ts
+++ browser/gamecontoller.ts
@@ -1,23 +1,31 @@
+import { Game } from "../common/game.js";
 import { GameState } from "../common/gamestate.js";
 import { Point } from "../common/point.js";
 export type GameOverHandler = () => void;
 export type GameStateChangeHandler = (gamestate: GameState) => void;
 export class GameController {
+    private game: Game;
     private gameOver?: GameOverHandler;
     private gameStateChanged?: GameStateChangeHandler;
     static intervalMs = 500; 
     private timerId?: number | NodeJS.Timer;
 
-    changeDirection(direction: Point): void {
+    constructor(game: Game) {
+        this.game = game;        
+    }
 
+    changeDirection(direction: Point): void {
+        this.game.setCurrentDirection(direction);
     }
 
     startGame(): void {
+        this.game.reset();
         this.timerId = setInterval(this.onTimer.bind(this), GameController.intervalMs);
     }
 
     private onGameOver() {
         clearInterval(this.timerId);
+        this.gameOver?.();
     }
 
     setGameOverHandler(gameOver?: GameOverHandler): void {
@@ -29,7 +37,14 @@ export class GameController {
     }
 
     private onTimer(): void {
-
+        if(this.game.over()) {
+            return;
+        }
+        this.game.advance();
+        this.gameStateChanged?.(this.game.getCurrentState());
+        if(this.game.over()) {
+            this.onGameOver();
+        }
     }
 
 }
